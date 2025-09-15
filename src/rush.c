f870584873dca5ac46ce59250a9d5251896889bf
Jack-Rodriguez <jaxrod02@gmail.com>
2025-09-15 18:54:54 +0000

    Finished CD built in function

--- src/rush.c
+++ src/rush.c
@@ -5,19 +5,25 @@
 
 #define MAXARGS 128 //sets max # of arguments to 128, 256 is the total characters of input so this number of args is not likely to be reached
 
+//this is our parsing function. It goes through the given input and seperates tokens by spaces, tabs, and newlines. It also returns the argc to be used in other functions.
 int make_token(char *line, char **args, int maxargs)
 {
-    
+    //initializes an argument counter
     int argc = 0;
+    //sets a pointer to the line
     char *p = line;
+    //sets a pointer to the token
     char *token;
 
+    //while the token isn't null, seperate the tokens by " \t\n" which is spaces, tabs, and newlines.
     while ((token = strsep(&p, " \t\n")) != NULL)
     {
+        //if the token is empty, skip it
         if(*token == '\0')
         {
             continue;
         }
+        //if the argument counter reaches one less than max arguments, we print an error. This shouldn't ever happen but better safe than sorry.
         if(argc >= maxargs - 1)
         {
             char error_message[30] = "An error has occurred\n";
@@ -25,9 +31,11 @@ int make_token(char *line, char **args, int maxargs)
             return -1;
         }
 
+        //token now iterates to the next token in args, using the argument counter to keep track of the index. this also increments the argument counter for us.
         args[argc++] = token;
     }
 
+    //we have to set the larst argument as null so that it can be used later. functions will need to know when it is done.
     args[argc] = NULL;
     return argc;
 }
@@ -41,10 +49,13 @@ int change_dir(char **args, int argc)
         write(STDERR_FILENO, error_message, strlen(error_message));
         return -1;
     }
+        //if there are 2 arguments, we change the working directory to the second argument
     else
     {
         chdir(args[1]);
     }
+
+    return 0;
 }
 
 int main(int argc, char *argv[]) {
@@ -103,6 +114,8 @@ int main(int argc, char *argv[]) {
         {
             change_dir(args, argc);
         }
+
+        printf("cwd: %s\n", getcwd(NULL, 0));
 
         // For now, do nothing with other input
         free(line);
