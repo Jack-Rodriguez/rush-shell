bc1e7c8bc229a0c13c2ff9f4ca65e26bbf2746d0
Jack-Rodriguez <jaxrod02@gmail.com>
2025-09-18 19:12:25 +0000

    Added comments and cleaned up main function

--- src/rush.c
+++ src/rush.c
@@ -66,36 +66,47 @@ int change_dir(char **args, int argc)
     return 0;
 }
 
+//this function handles whenever the path command is used in the console. It can create new paths or delete all current ones
 int path(char **args, int argc)
 {
+    //this frees all current paths
     for(int i = 0; i < pathc; i++)
     {
         free(paths[i]);
         paths[i] = NULL;
     }
 
+    //we dont really need this as pathc would work well on its own but I just prefer it so that I can return the value
     int counter = 0;
+    //also sets path counter to 0
     pathc = 0;
 
+    //this shouldnt even be possible but if there are no arguments, we print an error
     if (argc == 0)
     {
         char error_message[30] = "An error has occurred\n";
         write(STDERR_FILENO, error_message, strlen(error_message));
         exit(1);
     }
+    //if there are no arguments after path, then we can just return as is because we already cleared the paths
     else if (argc == 1)
     {
         return 0;
     }
+    //adds the new paths to the paths array
     else
     {
+        //while the counter is less than the argument count (minus one because we dont want to include the path command)
         while(counter < argc - 1) 
         {
+            //sets the current path index to be the next argument
             paths[counter] = strdup(args[counter + 1]);
+            //increments the counter
             counter++;
         }
     }
 
+    //returns the amount of paths. At the time of coding I don't know if this will be needed but better safe than sorry.
     return counter;
 }
 
@@ -147,15 +158,9 @@ int main(int argc, char *argv[])
             continue;
         }
 
-
-
         // Built-in "exit"
         if(strcmp(line, "exit") == 0) 
         {
-            for(int i = 0; i < pathc; i++)
-            {
-                printf("path %d: %s\n", i, paths[i]);
-            }
             free(line);
             exit(0);
         }
@@ -166,15 +171,13 @@ int main(int argc, char *argv[])
             change_dir(args, argc);
         }
 
+        // Built-in "path"
         if(strcmp(args[0], "path") == 0)
         {
             pathc = path(args, argc);
         }
 
-
-
-
-        // For now, do nothing with other input
+        
         free(line);
     }
 }
