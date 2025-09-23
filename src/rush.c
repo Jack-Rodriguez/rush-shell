76e813de95054f28f226a1d6a522c7291ac0cf54
Jack-Rodriguez <jaxrod02@gmail.com>
2025-09-23 22:20:52 +0000

    Finished comments, and cleaned up tests

--- src/rush.c
+++ src/rush.c
@@ -1,8 +1,12 @@
+#define _GNU_SOURCE
+#define _POSIX_C_SOURCE 200809L
+//had a ton of errors so i needed the above definitions to be added
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
+
 
 //sets max # of arguments to 128, 256 is the total characters of input so this number of args is not likely to be reached
 #define MAXARGS 128 
@@ -14,8 +18,10 @@ char *paths[MAXPATHS];
 //aamount of paths in the above array
 int pathc = 0;
 
+//this will store the executable path if there is one
 char full_path[1024];
 
+//stores the file that the output will be redirected to
 char output_file[1024];
 
 
@@ -29,6 +35,7 @@ int make_token(char *line, char **args, int maxargs)
     //sets a pointer to the token
     char *token;
 
+    //sets output file as empty
     output_file[0] = '\0';
 
     //while the token isn't null, seperate the tokens by " \t\n" which is spaces, tabs, and newlines.
@@ -44,6 +51,7 @@ int make_token(char *line, char **args, int maxargs)
         {
             char error_message[30] = "An error has occurred\n";
             write(STDERR_FILENO, error_message, strlen(error_message));
+            fflush(stderr);
             return -1;
         }
 
@@ -54,28 +62,33 @@ int make_token(char *line, char **args, int maxargs)
     //we have to set the larst argument as null so that it can be used later. functions will need to know when it is done.
     args[argc] = NULL;
 
+    //this will search for the > symbol so that any errors can be handeled
     for(int i = 0; i < argc; i++)
     {
         if(strcmp(args[i], ">") == 0)
         {
+            //if there are multiple > symbols or if there is more than one arguments to the right of >
             if(i != argc - 2 || strcmp(args[i+1], ">") == 0)
             {
                 char error_message[30] = "An error has occurred\n";
                 write(STDERR_FILENO, error_message, strlen(error_message));
+                fflush(stderr);
                 return -1;
             }
+             //no errors
             else
             {
+                //makes the output file whatever the last argument is
                 strncpy(output_file, args[i+1], sizeof output_file);
                 output_file[sizeof output_file - 1] = '\0';
 
-                
+                //ensures that execv doesnt use the > symbol or the file name
                 args[i] = NULL;
-
                 break; 
             }
         }
     }
+    //returns argument count
     return argc;
 }
 
@@ -87,11 +100,13 @@ int change_dir(char **args, int argc)
     {
         char error_message[30] = "An error has occurred\n";
         write(STDERR_FILENO, error_message, strlen(error_message));
+        fflush(stderr);
         return -1;
     }
         //if there are 2 arguments, we change the working directory to the second argument
     else
     {
+        //changes directory to the second argument
         chdir(args[1]);
     }
 
@@ -101,6 +116,18 @@ int change_dir(char **args, int argc)
 //this function handles whenever the path command is used in the console. It can create new paths or delete all current ones
 int path(char **args, int argc)
 {
+    //this will ensure that all passed arguments are executable
+    for(int i = 1; i < argc; i++)
+        {
+            if(access(args[i], X_OK) != 0)
+            {
+                char error_message[30] = "An error has occurred\n";
+                write(STDERR_FILENO, error_message, strlen(error_message));
+                fflush(stderr);
+                return pathc;
+            }
+        }
+    
     //this frees all current paths
     for(int i = 0; i < pathc; i++)
     {
@@ -118,6 +145,7 @@ int path(char **args, int argc)
     {
         char error_message[30] = "An error has occurred\n";
         write(STDERR_FILENO, error_message, strlen(error_message));
+        fflush(stderr);
         exit(1);
     }
     //if there are no arguments after path, then we can just return as is because we already cleared the paths
@@ -128,15 +156,7 @@ int path(char **args, int argc)
     //adds the new paths to the paths array
     else
     {
-        for(int i = 1; i < argc; i++)
-        {
-            if(access(args[i], X_OK) != 0)
-            {
-                char error_message[30] = "An error has occurred\n";
-                write(STDERR_FILENO, error_message, strlen(error_message));
-                return -1;
-            }
-        }
+        
         //while the counter is less than the argument count (minus one because we dont want to include the path command)
         while(counter < argc - 1) 
         {
@@ -170,6 +190,7 @@ int is_executable(char *cmd, char **paths, int pathc)
             //this checks if the path is executable. if so we return 0
             if(access(exPath, X_OK) == 0)
             {
+                //sets the full path to be the executable path
                 strncpy(full_path, exPath, sizeof full_path);
                 full_path[sizeof full_path - 1] = '\0';
                 return 0;
@@ -183,11 +204,15 @@ int is_executable(char *cmd, char **paths, int pathc)
 //this is my main
 int main(int argc, char *argv[]) 
 {
+    //we end up not using argv but I left it in just in case. It was giving me a warning so i added this line
+    (void)argv; 
 
+    //if there are more than one arguments, we print an error and exit
     if(argc > 1) 
     {
         char error_message[30] = "An error has occurred\n";
         write(STDERR_FILENO, error_message, strlen(error_message));
+        fflush(stderr);
         exit(1);
     }
 
@@ -195,15 +220,19 @@ int main(int argc, char *argv[])
     paths[0] = strdup("/bin");
     pathc = 1;
 
+    //main loop of shell
     while(1) 
     {
+        //prints the prompt
         printf("rush> ");
         fflush(stdout);
 
+        //collects user input
         char *line = NULL;
         size_t len = 0;
         ssize_t nread = getline(&line, &len, stdin);
 
+        //if there was no input, we go to the next iteration
         if (nread == -1) 
         { 
             free(line);
@@ -227,10 +256,20 @@ int main(int argc, char *argv[])
             free(line);
             continue;
         }
-
+        //___________________Built Ins_______________________
         // Built-in "exit"
-        if(strcmp(line, "exit") == 0) 
+        if(strcmp(args[0], "exit") == 0) 
         {
+            //if there is more/less than one argument, we print an error
+            if(argc != 1)
+            {
+                char error_message[30] = "An error has occurred\n";
+                write(STDERR_FILENO, error_message, strlen(error_message));
+                fflush(stderr); 
+                free(line);
+                continue;
+            }
+            //if there is only one argument (which is correct), we exit
             free(line);
             exit(0);
         }
@@ -244,47 +283,58 @@ int main(int argc, char *argv[])
         {
             pathc = path(args, argc);
         }
+        //-___________________Executables_______________________
         else
         {
             if(is_executable(args[0], paths, pathc) == 0)
             {
                 //I got most of this section from the chapter 5 lecture slides
+                //creates a child process
                 int rc = fork();
                 if (rc < 0) 
                 { 
                     char error_message[30] = "An error has occurred\n";
                     write(STDERR_FILENO, error_message, strlen(error_message));
+                    fflush(stderr);
                     exit(1);
                 }
                 //CHILD PROCESS
                 else if (rc == 0)
                 {
+                    //not sure why this was needed but it was the only way I could get "non built in" error handling to match that of the examples
                     args[0] = full_path;  
 
+                    //if there is an output file, we redirect the output to that file
                     if (output_file[0] != '\0') 
                     {
                         if (!freopen(output_file, "w", stdout)) 
                         { 
                             char error_message[30] = "An error has occurred\n";
                             write(STDERR_FILENO, error_message, strlen(error_message));
+                            fflush(stderr);
                             exit(1);
                         }
                     }
-                    
+                    //executes the command inputted by the user
                     execv(full_path, args);
+                    //if execv fails, the program will return here and print the error
                     char error_message[30] = "An error has occurred\n";
                     write(STDERR_FILENO, error_message, strlen(error_message));
+                    fflush(stderr);
                     exit(1);
                 }
+                //PARENT PROCESS
                 else 
                 { 
-                    int wc = wait(NULL);
+                    //ensures that the parent process waits for the child process to finish
+                    wait(NULL);
                 }
             }
             else
             {
                 char error_message[30] = "An error has occurred\n";
                 write(STDERR_FILENO, error_message, strlen(error_message));
+                fflush(stderr);
             }
         }
 
